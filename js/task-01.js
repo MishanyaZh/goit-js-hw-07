9744452faf94bf19e603390cf3550862548ef4a5
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-06-04 00:09:21 +0200

    1 finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,26 @@
 // console.log("hello)");
 
+//amount categories
+const categorRef = document.querySelector('#categories');
+
+const amountCategories = categorRef.children.length;
+console.log(`В списке ${amountCategories} категории`);
+
+//name and amount
+const totalAraysCategories = categorRef.children;
+// console.log(totalAraysCategories);
+const categories = [...totalAraysCategories];
+    
+categories.map(cat => {
+    console.log(`Категория: ${cat.children[0].textContent}`);
+    console.log(`Количество элементов: ${cat.children[1].children.length}`);
+});
+
+
+
+
+
+//old version
 // 1
 // const amountCatRef = document.querySelectorAll('li.item');
 // // console.log(amountCatRef.length);
@@ -7,32 +28,26 @@
 // console.log(`В списке ${amountCatRefLeng} категории`);
 
 //1.1
-const categorRef = document.querySelector('#categories');
-// console.log(categorRef.children.length);
-const amountCat = categorRef.children.length;
-console.log(`В списке ${amountCat} категории`);
+// //2
+// const itemRef = document.querySelectorAll('li.item');
+// // // console.log(itemRef[0]);
+
+// const titelFirstRef = itemRef[0].querySelector('h2');
+// console.log(`Категория: ${titelFirstRef.textContent}`);
+
+// const itemFirst = itemRef[0].querySelectorAll('li');
+// console.log(`Количество элементов: ${itemFirst.length}`);
 
 
-//2
-const itemRef = document.querySelectorAll('li.item');
-// // console.log(itemRef[0]);
+// const titelSecondRef = itemRef[1].querySelector('h2');
+// console.log(`Категория: ${titelSecondRef.textContent}`);
 
-const titelFirstRef = itemRef[0].querySelector('h2');
-console.log(`Категория: ${titelFirstRef.textContent}`);
-
-const itemFirst = itemRef[0].querySelectorAll('li');
-console.log(`Количество элементов: ${itemFirst.length}`);
+// const itemSecond = itemRef[1].querySelectorAll('li');
+// console.log(`Количество элементов: ${itemSecond.length}`);
 
 
-const titelSecondRef = itemRef[1].querySelector('h2');
-console.log(`Категория: ${titelSecondRef.textContent}`);
+// const titelThirdRef = itemRef[2].querySelector('h2');
+// console.log(`Категория: ${titelThirdRef.textContent}`);
 
-const itemSecond = itemRef[1].querySelectorAll('li');
-console.log(`Количество элементов: ${itemSecond.length}`);
-
-
-const titelThirdRef = itemRef[2].querySelector('h2');
-console.log(`Категория: ${titelThirdRef.textContent}`);
-
-const itemThird = itemRef[2].querySelectorAll('li');
-console.log(`Количество элементов: ${itemThird.length}`);
+// const itemThird = itemRef[2].querySelectorAll('li');
+// console.log(`Количество элементов: ${itemThird.length}`);
