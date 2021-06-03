a69fc167aa84a661fe7a6f3fb24ae9964c1c7b77
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-06-03 12:23:05 +0200

    prettier-zadcha1

---
+++ js/task-01.js
@@ -0,0 +1,33 @@
+// console.log("hello)");
+
+// 1
+const amountCatRef = document.querySelectorAll('li.item');
+// console.log(amountCatRef.length);
+const amountCatRefLeng = amountCatRef.length;
+console.log(`В списке ${amountCatRefLeng} категории`);
+
+
+// //2
+const itemRef = document.querySelectorAll('li.item');
+// // console.log(itemRef[0]);
+
+const titelFirstRef = itemRef[0].querySelector('h2');
+console.log(`Категория: ${titelFirstRef.textContent}`);
+
+const itemFirst = itemRef[0].querySelectorAll('li');
+console.log(`Количество элементов: ${itemFirst.length}`);
+
+
+const titelSecondRef = itemRef[1].querySelector('h2');
+console.log(`Категория: ${titelSecondRef.textContent}`);
+
+const itemSecond = itemRef[1].querySelectorAll('li');
+console.log(`Количество элементов: ${itemSecond.length}`);
+
+
+const titelThirdRef = itemRef[2].querySelector('h2');
+console.log(`Категория: ${titelThirdRef.textContent}`);
+
+const itemThird = itemRef[2].querySelectorAll('li');
+console.log(`Количество элементов: ${itemThird.length}`);
+
