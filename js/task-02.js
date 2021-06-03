9c86808b6db00c50a56926d248cf5672723f80cf
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-06-03 13:59:27 +0200

    zadacha-2

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+console.log("hello");
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsRef = document.querySelector('#ingredients');
+// console.log(ingredientsRef);
+
+const elements = ingredients.map(ingredient => {
+    const ingrEl = document.createElement('li');
+    ingrEl.textContent = ingredient;
+   // console.log(ingrEl);
+
+    return ingrEl;
+});
+
+ingredientsRef.append(...elements);
+//console.log(ingredientsRef);
+
