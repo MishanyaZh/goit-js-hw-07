a76ca94510bd12cd5fb5786d78789144cf59e889
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-06-07 20:31:01 +0200

    task-6+2version. in coment

--- js/task-06.js
+++ js/task-06.js
@@ -1,34 +1,55 @@
-const inputRef = document.querySelector("#validation-input");
-// console.dir(inputRef);
+//const inputRef = document.querySelector("#validation-input");
 
-// inputRef.addEventListener('focus', inputFocus);
-// function inputFocus(event) {
-//     console.log(`focus+`);
+//inputRef.addEventListener('blur', inputBlur);
+
+// function inputBlur(event) {
+      
+//     const value = event.currentTarget.value;
+
+//     if (value.length < 6 || value.length > 6) {
+//         event.currentTarget.classList.add('invalid');
+//     }
+//     else { event.currentTarget.classList.add('valid');
+//         if (event.currentTarget.classList.contains('invalid')) {
+//             event.currentTarget.classList.replace('invalid','valid');
+//         }
+//     }     
 // }
+
+
+//------ dataset -------
+
+const inputRef = document.querySelector("#validation-input");
 
 inputRef.addEventListener('blur', inputBlur);
 
 function inputBlur(event) {
-
-    // console.log(`focus-`);
-    // console.log(event.currentTarget.value);
-     
-    
+      
     const value = event.currentTarget.value;
 
-    if (value.length < 6 || value.length > 6) {
+    if (value.length < inputRef.dataset.length || value.length > inputRef.dataset.length) {
         event.currentTarget.classList.add('invalid');
     }
     else { event.currentTarget.classList.add('valid');
         if (event.currentTarget.classList.contains('invalid')) {
             event.currentTarget.classList.replace('invalid','valid');
         }
-        
-       // event.currentTarget.classList.toggle('valid');
-        //console.log('not good');
-    }
-        
+    }     
 }
 
 
+// //----- 3 -----
+
+// const inputRef = document.getElementById('validation-input')
+
+// inputRef.addEventListener('blur', inputBlur);
+
+// function inputBlur(event) {
+
+//     if (event.currentTarget.value.length < inputRef.dataset.length
+//         || event.currentTarget.value.length > inputRef.dataset.length) {
+//         inputRef.classList = 'invalid';
+
+//     } else inputRef.classList = 'valid';
+// }
 
